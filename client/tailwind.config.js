b98152788457e20e092c892d41243945e227b4ce
Mohammed Khan <abukhan363@gmail.com>
2024-01-16 12:13:50 -0500

    setup tailwindcss

---
+++ client/tailwind.config.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+
+const {nextui} = require("@nextui-org/react");
+
+export default {
+  content: [
+    "./src/**/*.{html,js}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+}
